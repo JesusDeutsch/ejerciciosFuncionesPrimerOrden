9e71684216bba265353ab4cc7c54a891ac25aba7
Jesus Aleman <167805629+JesusDeutsch@users.noreply.github.com>
2024-06-10 18:36:35 +0200

    second comment

--- main.js
+++ main.js
@@ -12,61 +12,61 @@ const products = [
 
 // Utiliza forEach para mostrar con console.log cada país del array de países.
 
-// countries.forEach(element => console.log(element));
+countries.forEach(element => console.log(element));
 
 // Utiliza forEach para mostrar con console.log cada nombre del array de nombres.
 
-// names.forEach(element => console.log(element));
+names.forEach(element => console.log(element));
 
 // Utiliza forEach para mostrar con console.log cada número del array de números.
 
-// numbers.forEach(element => console.log(element));
+numbers.forEach(element => console.log(element));
 
 // Utiliza map para crear un nuevo array cambiando cada país a mayúsculas en el array de países.
 
-// const countriesToUpperCase = countries.map((name) => name.toUpperCase())
+const countriesToUpperCase = countries.map((name) => name.toUpperCase())
 
-// console.log(countriesToUpperCase);
+console.log(countriesToUpperCase);
 
 // Utilice map para crear un array de longitudes de países a partir del array de países.
 
-// const longitudes = countries.map((element) => element.length)
-// console.log(longitudes);
+const longitudes = countries.map((element) => element.length)
+console.log(longitudes);
 
 // Usa map para crear un nuevo array cambiando cada número al cuadrado en el array de números.
 
-// const numbersSquare = numbers.map((number) => number**2)
-// console.log(numbersSquare);
+const numbersSquare = numbers.map((number) => number**2)
+console.log(numbersSquare);
 
 // Utilice map para cambiar cada nombre a mayúsculas en el array de nombres.
 
-// const namesUppperCase = names.map((name) => name.toUpperCase())
-// console.log(namesUppperCase);
+const namesUppperCase = names.map((name) => name.toUpperCase())
+console.log(namesUppperCase);
 
 // Utilice map para asignar el array de productos a sus correspondientes precios.
 
-// const prices = products.map((product) => product.price);
-//   console.log(prices);
+const prices = products.map((product) => product.price);
+  console.log(prices);
 
 // Utilice filter para filtrar los países que contienen land.
 
-// const contriesContainingLand = countries.filter((country) => country.includes('Land'));
-// console.log(contriesContainingLand);
+const contriesContainingLand = countries.filter((country) => country.includes('Land'));
+console.log(contriesContainingLand);
 
 // Utilice filter para filtrar los países que tienen seis caracteres.
 
-// const coutriesHaveSixLetters = countries.filter((country) => country.length === 6);
-// console.log(coutriesHaveSixLetters);
+const coutriesHaveSixLetters = countries.filter((country) => country.length === 6);
+console.log(coutriesHaveSixLetters);
 
 // Utilice filter para filtrar los países que contengan seis letras o más en el array de países.
 
-// const coutriesHaveSixLettersOrMore = countries.filter((country) => country.length >= 6);
-// console.log(coutriesHaveSixLettersOrMore);
+const coutriesHaveSixLettersOrMore = countries.filter((country) => country.length >= 6);
+console.log(coutriesHaveSixLettersOrMore);
 
 // Utilice filter para filtrar los países que empiezan por "E";
 
-// const countriesBeginningE = countries.filter((country) => country.includes('E'))
-// console.log(countriesBeginningE);
+const countriesBeginningE = countries.filter((country) => country.includes('E'))
+console.log(countriesBeginningE);
 
 // Utilice filter para filtrar sólo los precios con valores.
 
